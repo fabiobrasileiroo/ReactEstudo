01fa9b0bc3ddfdfe3967bade96b7216e6052b7ee
fabiobrasileiroo <fabio.h591@gmail.com>
2023-09-08 14:27:58 -0400

    primeria alteração no react

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,13 @@
+ import './App.css';
+
+function App() {
+  return (
+  <div className="App">
+    <h1>Olá React</h1>
+    <p>Meus primerio app</p>
+    <p>tudo certo</p>
+  </div>
+  );
+}
+
+export default App;
